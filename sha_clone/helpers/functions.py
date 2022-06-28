5f824c81f4796c4d69d74f2a066af0e61dbd5319
giorgi_matcharashvili <giorgi.matcharashvili2002@gmail.com>
2022-06-28 18:28:30 +0400

    feat(sha_clone): finish operations and functions

---
+++ sha_clone/helpers/functions.py
@@ -0,0 +1,31 @@
+from operations import *
+
+
+def sigma0(bits):
+    return xor([rotate_right(bits, 7), rotate_right(bits, 18), shift_right(bits, 3)])
+
+
+def sigma1(bits):
+    return xor([rotate_right(bits, 17), rotate_right(bits, 19), shift_right(bits, 10)])
+
+
+def u_sigma0(bits):
+    return xor([rotate_right(bits, 2), rotate_right(bits, 13), rotate_right(bits, 22)])
+
+
+def u_sigma1(bits):
+    return xor([rotate_right(bits, 6), rotate_right(bits, 11), rotate_right(bits, 25)])
+
+
+def choice(bits):
+    x, y, z = bits
+    result = [z[i] if v == '0' else y[i] for i, v in enumerate(x)]
+
+    return ''.join(result)
+
+
+def majority(bits):
+
+    result = ['0' if i.count('0') > 1 else '1' for i in zip(*bits)]
+
+    return ''.join(result)
