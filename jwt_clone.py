6a371312f8acbbba2ee8465df313d3deda3d9874
giorgi_matcharashvili <giorgi.matcharashvili2002@gmail.com>
2022-08-01 16:57:54 +0200

    feat(jwt): Make it more secure by adding private variables and attack cases

--- jwt_clone.py
+++ jwt_clone.py
@@ -7,26 +7,62 @@ SECRET = 'be7ea2ee647b6576d3ee1942410ebcf5'
 
 class JwtClone:
     def __init__(self, token: str = None, header: dict = None, payload: dict = None):
-        self.HS256 = HmacClone
+        self._HS256 = HmacClone
 
         if not token and not (header and payload):
-            raise TypeError
+            raise TypeError("__init__() missing required positional argument/s: 'token' or 'header' and 'payload'")
 
         if token:
-            self.token = token
-            self.header, self.payload, self.signature = token.split('.')
+            self._token = token
+            self._header, self._payload, self._signature = token.split('.')
         else:
-            self.header = b64encode(json.dumps(header, separators=[',', ':']))
-            self.payload = b64encode(json.dumps(payload, separators=[',', ':']))
-            self.signature = self.HS256(SECRET, self.header + '.' + self.payload).hexdigest()
+            self._header = b64encode(json.dumps(header, separators=[',', ':']))
+            self._payload = b64encode(json.dumps(payload, separators=[',', ':']))
+            self._signature = self._HS256(SECRET, self._header + '.' + self._payload).hexdigest()
 
-            self.signature = b64encode(self.signature)
+            self._signature = b64encode(self._signature)
 
-            self.token = self.header + '.' + self.payload + '.' + self.signature
+            self._token = self._header + '.' + self._payload + '.' + self._signature
+
+        # In case if attacker provides a non-valid signature
+        self.authenticate()
+
+        self.validate()
 
     def validate(self):
-        signature = self.HS256(SECRET, self.header + '.' + self.payload).hexdigest()
+        header = b64decode(self._header)
+        try:
+            header = json.loads(header)
+        except:
+            try:
+                header = json.loads(header[:-1])
+            except:
+                raise TypeError("header must be a dictionary type")
+
+        if not "typ" in header.keys() or not 'alg' in header.keys():
+            raise KeyError("header missing required key/s: 'typ', 'alg'")
+
+        if header['typ'] != "JWT":
+            raise TypeError("type of the token must be: 'JWT' ")
+
+        # In case if attacker tries None algorithm method
+        # To bypass JSON Web Token controls
+        if header['alg'] == 'none' or header['alg'] is None:
+            header['alg'] = 'HS256'
+            raise TypeError("algorithm of the token can't be a none")
+
+        # In case if attacker tries KID manipulation method to crack the JWT
+        if 'kid' in header.keys():
+            try:
+                int(header['kid'])
+            except:
+                header['kid'] = '0'
+                raise TypeError("value of the 'kid' must be a integer")
+
+    def authenticate(self):
+        signature = self._HS256(SECRET, self._header + '.' + self._payload).hexdigest()
         signature = b64encode(signature)
 
-        return self.signature == signature
-
+        if self._signature != signature:
+            raise Exception('JWT is not valid')
+        return True
